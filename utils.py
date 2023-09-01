3df1ed6c93f3e6e143bbdcbf2a1cdf68c408d74a
Denis Grigoriev <denisgrigoriev04@ya.ru>
2023-09-01 22:34:53 +0300

    new environ

--- utils.py
+++ utils.py
@@ -1,19 +1,26 @@
+import os
+
 DEFAULT_MESSAGE_TEMPLATE = "<s>{role}\n{content}</s>\n"
 DEFAULT_SYSTEM_PROMPT = "Ты — Агафья, русскоязычная студентка университета ГУАП. Ты разговариваешь с людьми и помогаешь им, пытаясь уговорить поступить в ГУАП"
-BOT_START_TEXT = '<s> bot'
+DEFAULT_BOT_START_TEXT = '<s> bot'
+
+IS_USE_CONTEXT = os.environ.get("BOT_IS_USE_CONTEXT", False)
+MESSAGE_TEMPLATE = os.environ.get("BOT_MESSAGE_TEMPLATE", DEFAULT_MESSAGE_TEMPLATE)
+SYSTEM_PROMPT = os.environ.get("BOT_SYSTEM_PROMPT", DEFAULT_SYSTEM_PROMPT)
+BOT_START_TEXT = os.environ.get("BOT_BOT_START_TEXT", DEFAULT_BOT_START_TEXT)
 
 class Conversation:
     def __init__(
         self,
         messages=[],
-        is_use_context=False,
-        message_template=DEFAULT_MESSAGE_TEMPLATE,
-        system_prompt=DEFAULT_SYSTEM_PROMPT,
+        is_use_context=IS_USE_CONTEXT,
+        message_template=MESSAGE_TEMPLATE,
+        system_prompt=SYSTEM_PROMPT,
         bot_start_text=BOT_START_TEXT,
     ):
         self.message_template = message_template
         self.bot_start_text = bot_start_text
-        self.is_use_context = is_use_context
+        self.is_use_context = is_use_context in [True, "True"]
         self.messages = [{ "role": "system", "content": system_prompt }]
         for m in messages:
             self.add_message(**m)
